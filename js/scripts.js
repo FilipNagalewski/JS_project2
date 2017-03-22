9548ac76def7040e1f7cce20c2ec91083e26c1b8
Filip Nagalewski <nagalewski@gmail.com>
2017-03-22 02:12:37 +0100

    triangle area calculator added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-// my first scripts.js
-
-var name = prompt('Filip');
-//alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
-
-/* that's all folks
-      ... for now */
+var a = 4,
+  h = 8,
+  triangleArea = alert(a*h/2);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
