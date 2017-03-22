508e960a73dbfd755dd5a300ed81f7030d3e30d0
Filip Nagalewski <nagalewski@gmail.com>
2017-03-22 17:10:40 +0100

    prompt added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // my first js script
 
-var a = 4, // some meaningless comment
-  h = 8,
-  triangleArea = alert(a*h/2);
+var a = prompt('Pole trójkąta o podstawie "a": '), // some meaningless comment
+  h =  prompt('... i wysokości "h": '),
+  triangleArea = alert('... wynosi: ' + a*h/2);
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 
 /* that's all folks!
