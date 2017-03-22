89d474795304db1d35577a569c42600c23fce5fe
Filip Nagalewski <nagalewski@gmail.com>
2017-03-22 09:40:27 +0100

    minor change

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // my first js script
 
-var a = 4,
+var a = 4, // some meaningless comment
   h = 8,
   triangleArea = alert(a*h/2);
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
